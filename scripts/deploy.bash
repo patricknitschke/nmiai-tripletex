ece72cf651d28719d541af4354285933c8f027e3
Patrick Nitschke <patrick.nitschke@sintef.no>
2026-03-21 00:14:14 +0100

    v09 build, openapi dockerfile fix

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -1,4 +1,4 @@
-gcloud run deploy pining-for-the-woods-tripletex-v08 \
+gcloud run deploy pining-for-the-woods-tripletex-v09 \
   --source . \
   --region europe-north1 \
   --allow-unauthenticated \
